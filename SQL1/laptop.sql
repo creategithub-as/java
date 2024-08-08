5360ecae4453dca1a977d03eb1d84f4f55f8aa97
ashwinikulkarni2102 <ashwinikulkarni.xworkz@gmail.com>
2024-08-08 12:51:23 +0530

    sql

--- SQL1/laptop.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE laptop;
-use laptop;
-CREATE TABLE universisy(id int,u_name varchar(20) unique,no_of_professor bigint, constraint professor_chk check(no_of_professor<20),state_name varchar(20),pincode int primary key);
-CREATE TABLE electronics(id int,electronics_name varchar(20) unique,serial_no bigint, constraint no_chk check(serial_no <100)
